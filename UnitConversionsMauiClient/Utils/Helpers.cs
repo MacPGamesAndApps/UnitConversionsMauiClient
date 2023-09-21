502b5f035bae865749c8bb3c9509bc0be89a7be6
Pablo Cafiso <pcypc@hotmail.com>
2023-09-20 22:41:44 -0400

    Completed the app and added multi-environment support.

--- UnitConversionsMauiClient/Utils/Helpers.cs
+++ UnitConversionsMauiClient/Utils/Helpers.cs
@@ -1,17 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
 
 namespace UnitConversionsMauiClient.Utils
 {
     internal static class Helpers
     {
+        private static readonly MobileHostEnvironment _mobileHostEnvironment = new()
+        {
+            EnvironmentName = Environments.Production
+        };
+
         public static string GetEndpointRootUrl()
         {
+            #if DEBUG
+                _mobileHostEnvironment.EnvironmentName = Environments.Development;
+            #endif
+
             string endpointRootUrl = string.Empty;
-            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") != "Production")
+            if (_mobileHostEnvironment.EnvironmentName == "Production")
             {
                 endpointRootUrl = Constants.ConversionServiceEndPoints.PROD_UNIT_CONVERSION_ENDPOINT;
             }
@@ -21,5 +27,13 @@ namespace UnitConversionsMauiClient.Utils
             }
             return endpointRootUrl;
         }
+    }
+
+    internal sealed class MobileHostEnvironment : IHostEnvironment
+    {
+        public string EnvironmentName { get; set; } = Environments.Development;
+        public string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ContentRootPath { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IFileProvider ContentRootFileProvider { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
 }
